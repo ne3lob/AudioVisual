26ac3927375c9bb376a7093368bd8e1041352eb2
ne3lob <neslob.wr@gmail.com>
2023-09-22 15:13:11 +0200

    Warja scene

--- Assets/WARJA/VolumeUpdater.cs
+++ Assets/WARJA/VolumeUpdater.cs
@@ -5,107 +5,74 @@ using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
 using UnityEngine.Video;
+using UnityEngine.VFX;
 
 public class VolumeUpdater : MonoBehaviour
 {
-    //VIDEOS
-    // [Header("VIDEOS")]
-    //
-    // [SerializeField] private GameObject Video1;
-    // [Range(0, 1)] [SerializeField] private float SpeedCloudsFantastic;
-    // [Range(0, 1)] [SerializeField] private float AlphaCloudsFantastic;
-    //
-    // [SerializeField] private GameObject Video2;
-    // [Range(0, 10)] [SerializeField] private float SpeedWITCHES;
-    // [Range(0, 1)] [SerializeField] private float AlphaWITCHES;
-    //
-    // [SerializeField] private GameObject Video3;
-    // [Range(9,10)] [SerializeField] private float SpeedYellow;
-    // [Range(0, 1)] [SerializeField] private float AlphaYellow;
-    //
-    // [SerializeField] private GameObject WaterPlane;
-    // [Range(0, 10)] [SerializeField] private float SpeedWater;
-    // [Range(0, 1)] [SerializeField] private float AlphaWater;
-    // [Range(0, 1)] [SerializeField] private float SmoothnessWater;
-    // private Material waterMaterial;
-    //
-    // private VideoPlayer video1player;
-    // private VideoPlayer video2player;
-    // private VideoPlayer video3player;
-    //
-    // // FLOWER
-    // [Header("FLOWER")]
-    //
-    // [SerializeField] private MeshRenderer[] Flowers;
-    // [Range(0,1)] [SerializeField] private float FlowerSpeed;
-    // [Range(0,1)] [SerializeField] private float FlowerAlpha;
-    //
-    // // OCEAN
-    // [Header("OCEAN")] 
-    //
-    // [SerializeField] private GameObject Ocean;
-    // private Material oceanMaterial;
-    // [Range(0.1f, 1f)] [SerializeField] private float OceanScale;
-    private InputManagerNovation inputScript;
-    // VOLUME
-    [Header("VOLUME")]
+    public VisualEffect vfx;
 
-    [SerializeField] private Volume Volume;
+    private InputManagerNovation inputScript;
+
+    // VOLUME
+    [Header("VOLUME")] [SerializeField] private Volume Volume;
     private Bloom bloom;
     private Vignette vignette;
     private WhiteBalance whiteBalance;
-    
+
     private ColorAdjustments colorAdjustments;
-    
-    [Range(0,1)] [SerializeField] private float bloomWeight;
-    [Range(-100,100)] [SerializeField] private float temperature;
+
+    [Range(0, 1)] [SerializeField] private float bloomWeight;
+    [Range(-100, 100)] [SerializeField] private float temperature;
     [Range(0, 1)] [SerializeField] private float vinigretteWeight;
     [Range(0, 100)] [SerializeField] private float dirtWeight;
-    [Range(0,1)] [SerializeField] private float scatterWeight;
-    
+    [Range(0, 1)] [SerializeField] private float scatterWeight;
+
     // Start is called before the first frame update
     void Start()
     {
         inputScript = GetComponent<InputManagerNovation>();
-       
+        //vfx = GetComponent<VisualEffect>();
     }
 
     // Update is called once per frame
     void Update()
     {
-     
         bloomWeight = inputScript.SendA1;
-        scatterWeight=  inputScript.SendA1;
-        dirtWeight= inputScript.SendA1*100;
+        scatterWeight = inputScript.SendA1;
+        dirtWeight = inputScript.SendA1 * 100;
         vinigretteWeight = inputScript.SendA1;
-        temperature = (inputScript.SendA1*200f)-100f;
+        temperature = (inputScript.SendA1 * 200f) - 100f;
+        vfx.SetFloat("Setsize", inputScript.SendA1 * 0.15f);
+        vfx.SetFloat("SetVectorVelocity", inputScript.SendA1 * 0.5f);
     }
 
     // VOLUME SETTINGS
-    
+
     private void FixedUpdate()
     {
         if (Volume.profile.TryGet<Bloom>(out bloom))
         {
             bloom.intensity.value = bloomWeight;
         }
+
         if (Volume.profile.TryGet<Bloom>(out bloom))
         {
             bloom.scatter.value = scatterWeight;
         }
+
         if (Volume.profile.TryGet<Bloom>(out bloom))
         {
-            bloom.dirtIntensity.value =dirtWeight;
+            bloom.dirtIntensity.value = dirtWeight;
         }
+
         if (Volume.profile.TryGet<Vignette>(out vignette))
         {
-            vignette.intensity.value =vinigretteWeight;
+            vignette.intensity.value = vinigretteWeight;
         }
+
         if (Volume.profile.TryGet<WhiteBalance>(out whiteBalance))
         {
             whiteBalance.temperature.value = temperature;
         }
-        
-      
     }
 }
